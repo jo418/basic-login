6b5e87ba36d2b096bcadbd1a5dd75955b27a1bbb
jo418 <jussi.opas@mavericks.fi>
2022-03-21 23:09:07 +0200

    use hooks && ask also server

--- src/components/login/LoginDialog.js
+++ src/components/login/LoginDialog.js
@@ -4,9 +4,16 @@ import { useState } from 'react';
 const KEY_NAME = 'loginData';
 
 function LoginDialog(props) {
-    const [loginData, setLoginData] =
-        useState(console.log(localStorage.getItem(KEY_NAME))
-            ? JSON.parse(localStorage.getItem(KEY_NAME)) : null);
+    const [loginData, setLoginData] = useState(console.log(localStorage.getItem(KEY_NAME))
+        ? JSON.parse(localStorage.getItem(KEY_NAME)) : null);
+
+    // Let's write this in a difficult way
+    const children = props.children;
+    const index = children.indexOf('updateAuth=') + 1;
+    const setAuthorization = children[index];
+    const handleAuthorization = toggle => {
+        setAuthorization(toggle);
+    }
 
     // Environment variable must be set to enable login.
     // Login button will stay as inactive if there is no correct client id.
@@ -14,7 +21,7 @@ function LoginDialog(props) {
     // It is wise to implement this differently in production.
     // With default id there will be Error 401: invalid_client.
     // With undefined, the button will be insensitive.
-    const clientId = process.env.REACT_APP_SEMI_SECRET_CLIENT_ID || "xyzhijk1";
+    const clientId = process.env.REACT_APP_PUBLIC_CLIENT_ID || "xyzhijk1";
 
     const handleFailure = (result) => {
         console.log("Failure " + JSON.stringify(result));
@@ -29,13 +36,34 @@ function LoginDialog(props) {
             token: token,
             email: email
         }
+        handleSendToServer(token);
         setLoginData(tokenData);
         localStorage.setItem(KEY_NAME, tokenData);
     };
 
+    const handleSendToServer = (token) => {
+        const url = process.env.REACT_APP_SERVER_URL;
+        if (url !== undefined) {
+            const data = { token: token };
+            fetch(url, {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(data)
+            }).then(response => {
+                handleAuthorization(response.status === 200);
+                return response.status;
+            }).catch(error => {
+                console.log('There was an error!', error);
+            });
+        }
+    }
+
     const handleLogout = () => {
         localStorage.removeItem(KEY_NAME);
         setLoginData(null);
+        handleAuthorization(false);
     };
 
     //console.log(KEY_NAME + '=', loginData);
@@ -60,3 +88,4 @@ function LoginDialog(props) {
 }
 
 export default LoginDialog;
+ 
